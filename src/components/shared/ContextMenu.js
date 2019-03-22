6ea02dc058f4f967ed0789272465b3adf5ae5251
Thomas White <im.toms.inbox@gmail.com>
2019-03-22 15:05:37 +1000

    Created shared ContextMenu component

---
+++ src/components/shared/ContextMenu.js
@@ -0,0 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// @flow
+import { PureComponent } from 'react';
+
+export default class ContextMenu extends PureComponent<{}> {
+
+}
